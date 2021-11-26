530dbfd9907597cff7bea021f503a1418e57de1e
DimaKrempoviy <krempoviy98@gmail.com>
2021-11-26 09:25:55 +0200

    tasks 13 and 15 are added

--- src/Task15.java
+++ src/Task15.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 
 public class Task15 {
     public static void main(String[] args) {
-        int num1, num2;
 
         Scanner i = new Scanner(System.in);
         System.out.println("Enter the num1: ");
-        int num1 = i.nextDouble();
+        double num1 = i.nextDouble();
 
         System.out.println("Enter the num2: ");
-        int num2 = i.nextDouble();
+        double num2 = i.nextDouble();
 
-        System.out.println();
+        System.out.println("num1 = " + num1 + ", " + "num2 = "+  num2 + " before replacement");
 
         num1 = num2 + num1;
         num2 = num1 - num2;
         num1 = num1 - num2;
 
+        System.out.println("num1 = " + num1 + ", " + "num2 = " + num2 + " after replacement");
 
 
     }
 }
+
+/*
+Write a Java program to swap two variables.
+ */
